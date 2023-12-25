8e255d5ed6c176f70d59fe6a652ad81dc4aeeac7
AnnaSolera <240312@studenti.unimore.it>
2023-12-25 19:44:14 +0100

    test communication client-server in flask

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
     return render_template("index.html")
+
+@app.route("/upload_data", methods=["GET", "POST"])
+def upload_data():
+    i = request.form["i"]
+    j = request.form["j"]
+    k = request.form["k"]
+    return i
